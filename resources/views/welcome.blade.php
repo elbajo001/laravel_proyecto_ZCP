b9bdb0eaebd6eacb42dc8fe81fdc3f59f03aab5d
lino <linomu@unicauca.edu.co>
2019-11-13 08:29:06 -0500

    prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Zorros
                 </div>
 
                 <div class="links">
