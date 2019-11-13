f555ebc44d0df6ceb33d94105292e131b78120bb
lino <linomu@unicauca.edu.co>
2019-11-13 08:30:08 -0500

    prueba on 'master'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Zorros
+                    Zorros Privativos Comunes
                 </div>
 
                 <div class="links">
